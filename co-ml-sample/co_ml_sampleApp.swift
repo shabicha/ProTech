51ee2c4f09ff3862fb1ff43f7a45637ab67d1044
Tiffany Tseng <tiffanytseng@apple.com>
2022-03-22 10:51:29 -0700

    Initial Commit

---
+++ co-ml-sample/co_ml_sampleApp.swift
@@ -0,0 +1,17 @@
+//
+//  co_ml_sampleApp.swift
+//  co-ml-sample
+//
+//  Created by Tiffany Tseng on 3/22/22.
+//
+
+import SwiftUI
+
+@main
+struct co_ml_sampleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
